9c0e9287b12cbd11cb6c7f4f495ba283ff3491f1
Rajesh <rajeshreddykumbam123@gmail.com>
2025-06-11 13:03:31 +0530

    String Reverse

---
+++ StringReverse.java
@@ -0,0 +1,14 @@
+public class StringReverse {
+    public static void main(String[] args) {
+        System.out.println("-----Stringbuffer Example-----");
+        StringBuffer str= new StringBuffer("Rajesh");
+        System.out.println(str.reverse());
+        System.out.println("-----String Example-----");
+        String arr = "java";
+        char[] rev=new char[arr.length()];
+        for(int i=1;i<=arr.length();i++){
+            rev[i-1]=arr.charAt(arr.length()-i);
+        }
+        System.out.println(rev);
+    }
+}
