6a7980b0ebe5f37d3ebfb271eaf15a341347dd8d
Rajesh <rajeshreddykumbam123@gmail.com>
2025-06-15 23:08:12 +0530

    Set Example'

---
+++ Set/Myset.java
@@ -0,0 +1,17 @@
+package Set;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Myset {
+    public static void main(String[] args) {
+        Set<Integer> num = new HashSet<Integer>();
+        num.add(4);
+        num.add(7);
+        num.add(9);
+        num.add(5);
+        num.add(4);//Unique elements to be added
+        System.out.println(num);// no specific order for retrivel
+    }
+}
