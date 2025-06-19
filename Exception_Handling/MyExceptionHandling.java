e31ff96f7f6ad4528a6f6179228c5fc5de85997d
Rajesh <rajeshreddykumbam123@gmail.com>
2025-06-19 23:25:49 +0530

    Exceptional Handling

--- Exception_Handling/MyExceptionHandling.java
+++ Exception_Handling/MyExceptionHandling.java
@@ -20,11 +20,17 @@ public class MyExceptionHandling {
         } catch (ArithmeticException e) {
             System.out.println("Not divisible with zero");
         }
+        finally{
+            System.out.println("Finally block is executed");// this block is always executes
+        }
 
         try {
             obj.access();
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Access outside the limit");
+        }
+         finally{
+            System.out.println("Finally block is executed");//usesfull for closing the opened resources
         }
     }
 }
