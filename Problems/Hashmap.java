e85384c7340e13fd4261c958ebbd5b12c6010a73
Rajesh <rajeshreddykumbam123@gmail.com>
2025-06-20 23:28:53 +0530

    Hashmap problem

---
+++ Problems/Hashmap.java
@@ -0,0 +1,13 @@
+package Problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashmap {
+    public static void main(String[] args) {
+        Map<String,Integer> mp = new HashMap<>();
+        mp.put("Rajesh", 21);
+        mp.put("Suresh", 28);
+        System.out.println(mp);
+    }
+}
