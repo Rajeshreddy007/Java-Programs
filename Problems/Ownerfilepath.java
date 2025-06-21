e22a6aee1ab2203fb03e4fca0e933b1f315c03eb
Rajesh <rajeshreddykumbam123@gmail.com>
2025-06-21 19:27:05 +0530

    NEW CHANGES

--- Problems/Ownerfilepath.java
+++ Problems/Ownerfilepath.java
@@ -2,10 +2,11 @@ package Problems;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Ownerfilepath {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         File file = new File("C:\\Users\\Rajes\\Desktop\\JavaCourse\\Problems\\exp.txt");
         System.out.println(file.getPath());
 
